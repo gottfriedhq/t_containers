1834e15317cf1ddb8130940004951cec3fdf6097
Gottfried Ansah <gottfried.ansah@gmail.com>
2023-04-10 12:48:11 +0000

    AppBar & Rectangular Container

--- lib/home-page.dart
+++ lib/home-page.dart
@@ -6,14 +6,18 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          decoration: BoxDecoration(
-              color: Color.fromARGB(255, 255, 193, 7),
-              borderRadius: BorderRadius.circular(20)),
-        ),
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.amber,
+        shadowColor: Colors.white,
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20),
+        width: 400,
+        height: 200,
+        decoration: BoxDecoration(
+            color: Color.fromARGB(255, 255, 193, 7),
+            borderRadius: BorderRadius.circular(10)),
       ),
     );
   }
